91de74d38ea6bbfa0462c260aba3df725ee57713
thecyclingfarmer <martgust@gmail.com>
2016-02-29 12:09:26 +0100

    jslint

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var edwise = require('./lib/edwise'),
-    dotenv = require('dotenv');
+var edwise = require('./lib/edwise');
+var dotenv = require('dotenv');
 dotenv.load();
-
 
 var connection = edwise.createConnection({
     username:process.env.USER_NAME,
-    password: process.env.PASSWORD
+    password: process.env.PASSWORD,
   });
 
-connection.login().then(function (res) {
+connection.login().then(function(res) {
   console.log('Authenticated response', res);
 });
